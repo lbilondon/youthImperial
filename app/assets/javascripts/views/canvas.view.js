45e19baee2dcd197e9dee801ebda0cbcff6e96d3
Andy Davies <andy.davies@gmail.com>
2012-05-03 15:08:50 +0100

    blending two images together with a filter in the middle

--- app/assets/javascripts/views/canvas.view.js
+++ app/assets/javascripts/views/canvas.view.js
@@ -12,7 +12,7 @@ define([
 			template : _.template(CanvasTemplate),
 			
 			initialize : function() {
-				_.bindAll(this, 'render', 'getImageData');
+				_.bindAll(this, 'render', 'getImageData', 'manipulateImage');
 				this.render();
 			},
 			
@@ -20,49 +20,121 @@ define([
 				this.$el.append(this.template());
 				
 				this.canvas = this.$el.find('#feedCanvas')[0];
-				this.ctx = this.canvas.getContext('2d');
-				
-				var model = new UserModel();
-				
-				var that = this;
-				model.fetch({
-					success: function (rModel) {
-						
-						function downloadCallback(imageData) {
-//							var finalImage = that.canvas.toDataURL("image/png");
-//							that.$el.append('<a href="' + finalImage + '">download image</a>');
-							console.log(imageData.data[4]);
-						}
-						
-						var img1_src = 'data:image/jpg;base64,' + rModel.get('user').feed[0];
-						var img2_src = 'data:image/jpg;base64,' + rModel.get('yi').feed[0];
-						
-						that.getImageData(img1_src, downloadCallback);
-						that.getImageData(img2_src, downloadCallback);
+				if (this.canvas !== undefined) {
+					this.ctx = this.canvas.getContext('2d');
 
-					}
-				});
+					var model = new UserModel();
+					
+					var that = this;
+					model.fetch({
+						success: function (rModel) {
+							
+							var img1_src = 'data:image/jpg;base64,' + rModel.get('user').feed[0];
+							var img2_src = 'data:image/jpg;base64,' + rModel.get('yi').feed[0];
+							
+							that.getImageData(img1_src, img2_src, that.manipulateImage);
+	
+						}
+					});
+				}
 
 				return this;
 			},
 			
-			getImageData: function(imageSrc, callback) {
-				var imageHeight = 615,
-					imageWidth = 615;
-				
-				this.canvas.height = imageHeight;
-				this.canvas.width = imageWidth;
-				this.ctx.clearRect(0, 0, imageWidth, imageHeight);
+			getImageData: function(image1Src, image2Src, callback) {
+				var height = 615,
+					width = 615,
+					img1Loaded = false,
+					img2Loaded = false,
+					img1Data = null,
+					img2Data = null;
 
-				var img = new Image();
+				this.canvas.height = height;
+				this.canvas.width = width;
+				this.ctx.clearRect(0, 0, width, height);
+
+				var img1 = new Image();
 				var that = this;
-				img.onload = function () {
-					that.ctx.drawImage(img, 0, 0, imageWidth, imageHeight);
-					var imageData = that.ctx.getImageData(0, 0, imageWidth, imageHeight);
-					that.ctx.clearRect(0, 0, imageHeight, imageWidth);
-					callback(imageData);
+				img1.onload = function () {
+					that.ctx.drawImage(img1, 0, 0, width, height);
+					img1Data = that.ctx.getImageData(0, 0, width, height);
+					that.ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
+					img1Loaded = true;
+					if (img1Loaded && img2Loaded) {
+						callback(img1Data, img2Data);	
+					}
 				}
-				img.src = imageSrc;
+				img1.src = image1Src;
+
+				var img2 = new Image();
+				img2.onload = function () {
+					that.ctx.drawImage(img2, 0, 0, width, height);
+					img2Data = that.ctx.getImageData(0, 0, width, height);
+					that.ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
+					img2Loaded = true;
+					if (img1Loaded && img2Loaded) {
+						callback(img1Data, img2Data);	
+					}
+				}
+				img2.src = image2Src;
+			},
+			
+			manipulateImage: function(image1Data, image2Data) {
+
+				var pixels = 4 * this.canvas.width * this.canvas.height;
+				var tmp = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+				while (pixels--) {
+					tmp.data[pixels] = (image1Data.data[pixels] * 0.5) + (image2Data.data[pixels] * 0.5);
+				}
+
+				this.ctx.putImageData(tmp, 0, 0);
+				var image3Data = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+
+				pixels = 4 * this.canvas.width * this.canvas.height;
+				var finalImage = image3Data;
+				var pixel = this.canvas.width * this.canvas.height;
+				var row = this.canvas.height;
+				var column = this.canvas.width;
+				var colPos1 = Math.round((this.canvas.width/100)*45);
+				var colPos2 = Math.round((this.canvas.width/100)*75);
+
+				var freq = 5;
+				var thisFreq = freq;
+
+				
+				for (var x = pixels; x >= 0; x = x-4) {
+					column--;
+					if (column <= 0) {
+						column = this.canvas.width;
+						row--;
+						freq--;
+						if (freq <= 0) {
+							colPos1--;
+							colPos2--;
+							freq = thisFreq;
+						}
+					}
+					
+					if (column < colPos1) {
+						finalImage.data[x] = image1Data.data[x];
+						finalImage.data[x-1] = image1Data.data[x-1];
+						finalImage.data[x-2] = image1Data.data[x-2];
+						finalImage.data[x-3] = image1Data.data[x-3];
+					}
+
+					if (column > colPos2) {
+						finalImage.data[x] = image2Data.data[x];
+						finalImage.data[x-1] = image2Data.data[x-1];
+						finalImage.data[x-2] = image2Data.data[x-2];
+						finalImage.data[x-3] = image2Data.data[x-3];
+					}
+				}
+
+				this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+				this.ctx.putImageData(finalImage, 0, 0);
+
+				var finalImageUrl = this.canvas.toDataURL("image/png");
+				this.$el.append('<a href="' + finalImageUrl + '">download image</a>');
 			}
 		});
 	}
