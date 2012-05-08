f288543c544efee78c0ddafeb05af16e4a69a384
Andy Davies <andy.davies@gmail.com>
2012-05-08 14:50:37 +0100

    still trying to fix heroku push issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,6 @@ YouthImperial::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += %w( modernizr.js )
   
   # Generate digests for assets URLs
   config.assets.digest = true
