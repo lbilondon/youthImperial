25a9770969f8878187fa84e0eaff1f9de42979e3
Andy Davies <andy.davies@gmail.com>
2012-05-08 15:31:20 +0100

    still debugging heroku issues

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module YouthImperial
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
     config.assets.precompile += %w( modernizr.js )
   end
 end
