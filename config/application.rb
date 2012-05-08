89482a52ff95b5b2e1971a07195760a5ec1d5c0f
Andy Davies <andy.davies@gmail.com>
2012-05-08 16:45:29 +0100

    heroku

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module YouthImperial
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = true
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
